4c6435b6697150c588c8d406b75bc50a4f5f6724
Danco21 <39180205+Danco21@users.noreply.github.com>
2022-03-18 21:59:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function resizeCanvas(){
   swirl1_surface.height = window.innerHeight;
   
 }
+
+function draw(){
+ let swirl1_surface = swirl1_surface.getContext("2d");
+ let width = swirl1_surface.width * 0.2;
+ let height = swirl1_surface.height * 0.2;
+ let xpos = swirl1_surface.width/2 - width/2;
+ let ypos = swirl1_surface.height/2 - height/2;
+  
+}
