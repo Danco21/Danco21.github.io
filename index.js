98d2603fff4721379021499bdf551ff4db17df87
Danco21 <39180205+Danco21@users.noreply.github.com>
2022-03-18 21:14:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let swirl1_surface;
 
+window.onload = function(){
+ console.log('loading.'); 
+  swirl1_surface = document.getElementById("swirl1_surface");
+  swirl1_surface.style.backgroundColor = "#282a36";
+  resizeCanvas();
+}
+window.onresize = function(){
+ console.log('resizing.'); 
+  resizeCanvas();
+
+}
+function resizeCanvas(){
+ swirl1_surface.width = window.innerWidth;
+  swirl1_surface.height = window.innerHeight;
+  
+}
